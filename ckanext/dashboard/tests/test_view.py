fe87ce3020f0a1b2b6c4a24fab6c382dca953ad2
Vitor Baptista <vitor@vitorbaptista.com>
2014-07-03 17:43:11 -0300

    [#14] We don't depend on viewhelpers anymore

--- ckanext/dashboard/tests/test_view.py
+++ ckanext/dashboard/tests/test_view.py
@@ -7,11 +7,9 @@ class TestDashboard(object):
 
     @classmethod
     def setup_class(cls):
-        p.load('viewhelpers')
         p.load('dashboard_preview')
         cls.plugin = p.get_plugin('dashboard_preview')
 
     @classmethod
     def teardown_class(cls):
         p.unload('dashboard_preview')
-        p.unload('viewhelpers')
