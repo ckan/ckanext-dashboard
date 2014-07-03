02660cc385aa52b079cc3377111d1bf10859022c
Vitor Baptista <vitor@vitorbaptista.com>
2014-07-03 14:53:24 -0300

    [#14] Remove filters UI
    This code was moved to CKAN core in issue #1792.

--- ckanext/dashboard/plugin.py
+++ ckanext/dashboard/plugin.py
@@ -40,10 +40,9 @@ class DashboardView(p.SingletonPlugin):
                 'title': 'Dashboard',
                 'icon': 'dashboard',
                 'iframed': False,
+                'filterable': True,
                 'schema': {'json': [ignore_empty, unicode],
                            'added_view_id': [ignore],
-                           'user_filter_names': [ignore_missing],
-                           'user_filter_fields': [ignore_missing]
                           },
                 'preview_enabled': True,
                 'full_page_edit': True,
@@ -108,29 +107,11 @@ class DashboardView(p.SingletonPlugin):
 
         resource = data_dict['resource']
         resource_view = data_dict['resource_view']
-        resource_view = self._filter_fields_and_names_as_list_without_duplicates(resource_view)
-
-        filters_enabled = resource.get('datastore_active', False)
 
         result = {
             'available_views': resource_views,
             'current_dashboard': current_dashboard,
-            'filters_enabled': filters_enabled,
         }
-
-        if filters_enabled:
-            dropdown_values = get_filter_values(resource)
-            fields = _get_fields(resource)
-            field_label_mapping = self._get_field_to_label_mapping(resource_view)
-            current_dropdown_values = self._get_dropdown_values(resource_view)
-
-            result.update({
-                'fields': fields,
-                'dropdown_values': dropdown_values,
-                'field_label_mapping': field_label_mapping,
-                'current_dropdown_values': current_dropdown_values
-            })
-
 
         return result
 
@@ -147,82 +128,6 @@ class DashboardView(p.SingletonPlugin):
                 resource_views[resource['name']].append(view)
 
         return resource_views
-
-    def _get_field_to_label_mapping(self, resource_view):
-        user_filter_fields = resource_view['user_filter_fields']
-        user_filter_names = resource_view['user_filter_names']
-        field_name_mapping = {}
-        for field, name in zip(user_filter_fields, user_filter_names):
-            if name:
-                field_name_mapping[field] = name
-            else:
-                if field not in field_name_mapping:
-                    field_name_mapping[field] = field
-
-        return field_name_mapping
-
-    def _get_dropdown_values(self, resource_view):
-        user_filter_fields = resource_view['user_filter_fields']
-
-        # first we pad out the values with the filters defined in the
-        # dashboard form.
-        current_dropdown_values = {}
-        for field in user_filter_fields:
-            if field in current_dropdown_values:
-                current_dropdown_values[field].append('')
-            else:
-                current_dropdown_values[field] = ['']
-
-        # then we actually add the data from the url.
-        for field, values in parse_filter_params().items():
-            # do not show dropdowns for fields not defined in data
-            if field not in user_filter_fields:
-                continue
-            # add empty value in the end, so the user can add more filters
-            for num, value in enumerate(values + ['']):
-                try:
-                    current_dropdown_values[field][num] = value
-                except IndexError:
-                    # extend the data if the parameters have more than
-                    # what is defined in the form.
-                    current_dropdown_values[field].append(value)
-
-        return current_dropdown_values
-
-    def _filter_fields_and_names_as_list_without_duplicates(self, resource_view):
-        filter_fields = resource_view.get('user_filter_fields')
-        filter_names = resource_view.get('user_filter_names')
-
-        if filter_fields is None:
-            filter_fields = []
-        elif isinstance(filter_fields, basestring):
-            filter_fields = [filter_fields]
-
-        if filter_names is None:
-            filter_names = []
-        elif isinstance(filter_names, basestring):
-            filter_names = [filter_names]
-
-        resource_view['user_filter_fields'] = filter_fields
-        resource_view['user_filter_names'] = filter_names
-
-        return self._remove_duplicate_filter_fields_and_names(resource_view)
-
-    def _remove_duplicate_filter_fields_and_names(self, resource_view):
-        filter_fields = resource_view['user_filter_fields']
-        filter_names = resource_view['user_filter_names']
-
-        for i, filter_field in enumerate(filter_fields):
-            indices = [i for i, field in enumerate(filter_fields)
-                       if field == filter_field]
-            for index in reversed(indices[1:]):
-                del filter_fields[index]
-                del filter_names[index]
-
-        resource_view['user_filter_fields'] = filter_fields
-        resource_view['user_filter_names'] = filter_names
-
-        return resource_view
 
 
 def parse_filter_params():
@@ -247,36 +152,3 @@ def _get_fields(resource):
 
     return [{'value': field['id']} for field in result.get('fields', []) if
             field['type'] in ['text', 'timestamp']]
-
-def get_filter_values(resource):
-    ''' Tries to get out filter values so they can appear in dropdown list.
-    Leaves input as text box when the table is too big or there are too many
-    distinct values.  Current limits are 5000 rows in table and 500 distict
-    values.'''
-
-    if not resource.get('datastore_active'):
-        return {}
-
-    data = {
-        'resource_id': resource['id'],
-        'limit': 5001
-    }
-    result = p.toolkit.get_action('datastore_search')({}, data)
-    # do not try to get filter values if there are too many rows.
-    if len(result.get('records', [])) == 5001:
-        return {}
-
-    filter_values = {}
-    for field in result.get('fields', []):
-        if field['type'] != 'text' and field['type'] != 'timestamp':
-            continue
-        distinct_values = set()
-        for row in result.get('records', []):
-            distinct_values.add(row[field['id']])
-        # keep as input if there are too many distinct values.
-        if len(distinct_values) > 500:
-            continue
-        filter_values[field['id']] = [{'id': value, 'text': value}
-                                      for value
-                                      in sorted(list(distinct_values))]
-    return filter_values
